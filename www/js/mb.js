7fc583dba02c24b5733ef590c1dce2ef8bc009ea
k21rs141 <k21rs141@st.kyusan-u.ac.jp>
2022-12-14 01:48:14 +0900

    Project's Initial Commit

---
+++ www/js/mb.js
@@ -0,0 +1,3 @@
+// This is a JavaScript file
+
+var ncmb = new NCMB("33bc098c6ba261a94fea22f7668093c2e2934a4c0eab28f27118a3045b2a6be2","8069ecff75c8430e1153c2b8767b77160e884ba43298df62a21b3d17c3ab2cb4");
